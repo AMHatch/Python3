14aa69a3ce269dcd866b1a4a1a256cb244ebb004
AndrewHatch <03110h@gmail.com>
2021-09-08 12:34:20 -0400

    added strings

--- strings.py
+++ strings.py
@@ -1,2 +1,26 @@
 #strings
 # Learning Python Strings
+# greet = 'Hello World, I\'m a new developer'
+# print(greet)
+# catvar = 'My name is '
+# name = 'Andrew'
+# print(catvar + name)
+# birth = 1992 
+# year = 2021
+# age = year - birth
+# print('I am ' + str(age) + ' years old.' )
+
+
+first_name = 'Andrew'
+last_name = 'Hatch'
+
+# preamp = 'My name is '
+
+
+# statement = preamp + first_name + last_name 
+# print(statement)
+
+# Hello Andrew Hatch, how are you doing today?
+
+sentence ='Hello {} {}, How are you doing today?'.format(first_name, last_name)
+print(sentence)
