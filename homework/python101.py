b003d4e51301e123d1bc8415160facf9129dad19
AndrewHatch <03110h@gmail.com>
2021-09-08 17:29:39 -0400

    problems 1 through 3

---
+++ homework/python101.py
@@ -0,0 +1,67 @@
+#Problem 1
+
+
+# bill = float(input("How much was the meal? :"))
+
+# service_input = input("How was the service? Good? Fair? or Bad?:")
+
+# service = service_input.lower()
+
+# if service == 'good':
+#     tip = bill * 0.2  
+# elif service == 'fair':
+#     tip = bill * 0.15
+# elif service == 'bad':
+#     tip = bill * 0.1
+# else:
+#     print("That's not a valid Input. Please choose between Good, Fair and Bad.")
+
+# total = tip + bill
+# "%.2f" % tip
+# "%.2f" % total
+# print('Your tip is $' + str("%.2f" % tip))
+# print('Your total is $' + str("%.2f" % total))
+
+
+
+#Problem 2 
+
+# bill = float(input("How much was the meal? :"))
+
+# service_input = input("How was the service? Good? Fair? or Bad?:")
+
+# split = input("how many people ate?")
+
+# service = service_input.lower()
+
+# if service == 'good':
+#     tip = bill * 0.2  
+# elif service == 'fair':
+#     tip = bill * 0.15
+# elif service == 'bad':
+#     tip = bill * 0.1
+# else:
+#     print("That's not a valid Input. Please choose between Good, Fair and Bad.")
+
+# total = tip + bill
+
+# split_total = float(total) / float(split)
+
+# print('The tip is $' + str("%.2f" % tip))
+# print('The total is $' + str("%.2f" % total))
+# print('Each person pays $' + str("%.2f" % split_total))
+
+#Problem 3
+
+# answer = 'yes'
+# coins = 0
+
+# while answer == 'yes':
+#     print('You have ' + str(coins) + ' coins!')
+#     answer_input = input('Do you want another?')
+#     answer = answer_input.lower()
+#     coins += 1
+#print('Bye!')
+
+#problem 4
+
