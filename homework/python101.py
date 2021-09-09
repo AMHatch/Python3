8c412fa973b895dd2de190dc3099511c520edaaf
AndrewHatch <03110h@gmail.com>
2021-09-09 11:02:17 -0400

    refactored

--- homework/python101.py
+++ homework/python101.py
@@ -29,7 +29,7 @@
 
 # service_input = input("How was the service? Good? Fair? or Bad?:")
 
-# split = input("how many people ate?")
+# split = input("How many people ate?")
 
 # service = service_input.lower()
 
@@ -41,14 +41,15 @@
 #     tip = bill * 0.1
 # else:
 #     print("That's not a valid Input. Please choose between Good, Fair and Bad.")
+#     exit()
 
 # total = tip + bill
 
-# split_total = float(total) / float(split)
+# split_total = float(total / split)
 
-# print('The tip is $' + str("%.2f" % tip))
-# print('The total is $' + str("%.2f" % total))
-# print('Each person pays $' + str("%.2f" % split_total))
+# print(f'The tip is ${tip:.2f}')
+# print(f'The total is ${total:.2f}')
+# print(f'Each person pays ${split_total:.2f}')
 
 #Problem 3
 
@@ -56,11 +57,11 @@
 # coins = 0
 
 # while answer == 'yes':
-#     print('You have ' + str(coins) + ' coins!')
-#     answer_input = input('Do you want another?')
+#     print(f'You have {coins} coins!')
+#     answer_input = input('Do you want another coin?')
 #     answer = answer_input.lower()
 #     coins += 1
-#print('Bye!')
+# print('Bye!')
 
 #problem 4
 
@@ -93,6 +94,8 @@
 # count = 1
 # gap_count = 3
 
+
+
 # while count < 11 and gap_count >= 0:
 #     print(gap * gap_count + '*' * count)
 #     count += 2
@@ -108,7 +111,7 @@
 #     b = 1
 #     while b <= 10:
 #         c = (a * b)
-#         print(str(a) + " x " + str(b) + " = " + str(c))
+#         print(f'{a} x {b} = {c}')
 #         b += 1
 #     print('')
 #     a += 1
